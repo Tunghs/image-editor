e770e05cb0b9011ed7b4c5bb6763dd47745e2989
Tunghs <juk08843@gmail.com>
2025-01-20 11:26:30 +0900

    Feat: "Apply WPF-UI"

---
+++ src/ImageEditor.UI/ShellWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Wpf.Ui.Controls;
+
+namespace ImageEditor.UI
+{
+    /// <summary>
+    /// ShellWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ShellWindow : FluentWindow
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
